ea7c3898798bb8cbc552472fcbf1161e15462ad6
ERR0RW0LF <finn.woma@gmail.com>
2024-07-01 16:24:01 +0200

    chore: Update myfirstprogram.cpp to include a variable and print its value and change it and print its new value
    Signed-off-by: ERR0RW0LF <finn.woma@gmail.com>

--- first projects/myfirstprogram.cpp
+++ first projects/myfirstprogram.cpp
@@ -5,7 +5,9 @@ int main() {
     // This is a comment
     cout << "Hello World!" << "\n\n";
     cout << "I am learning C++" << endl;
-    int myNum = 15;
-    cout << myNum;
+    int myNum = 15;  // myNum is 15
+    cout << "first: " << myNum << endl;
+    myNum = 10;  // Now myNum is 10
+    cout << "now: " << myNum;  // Outputs 10
     return 0;
 }
