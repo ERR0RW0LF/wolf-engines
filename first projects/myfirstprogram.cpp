745c5eeb13c664017b29c37c6b95c58238d08220
ERR0RW0LF <finn.woma@gmail.com>
2024-07-01 16:49:07 +0200

    chore: Update myfirstprogram.cpp to include user input and display the input value
    Signed-off-by: ERR0RW0LF <finn.woma@gmail.com>

--- first projects/myfirstprogram.cpp
+++ first projects/myfirstprogram.cpp
@@ -4,10 +4,14 @@ using namespace std;
 int main() {
     // This is a comment
     cout << "Hello World!" << "\n\n";
-    cout << "I am learning C++" << endl;
+    cout << "I am learning C++" << "\n\n";
     int myNum = 15;  // myNum is 15
-    cout << "first: " << myNum << endl;
+    cout << "first: " << myNum << "\n";
     myNum = 10;  // Now myNum is 10
-    cout << "now: " << myNum;  // Outputs 10
+    cout << "now: " << myNum << "\n\n";  // Outputs 10
+    int x;
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> x; // Get user input from the keyboard
+    cout << "\n" << "Your number is: " << x; // Display the input value
     return 0;
 }
